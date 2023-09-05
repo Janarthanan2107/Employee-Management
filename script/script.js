e623357d2a5e2b9dbe6045b7f989491d1eb2888e
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2023-09-05 19:59:35 +0530

    login screen along with js

--- script/script.js
+++ script/script.js
@@ -1,23 +1,75 @@
-// Using javascript file as strict file for exposing all the loss errors or eliminating the silent errors into throwable errors
 'use strict';
 
-//selecting every elements from the html files in two types inputs(providing values) and outputs(displaying)
 //inputs
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 const link = document.getElementById('homeLink');
 const button = document.getElementById('loginButton');
+const overlayContainer = document.getElementById('overlay')
+const modelContainer = document.getElementById('model-container')
+const closeBtn = document.getElementById('btn-close')
+
+const contentInDialog = document.getElementById("content")
+const loader = document.getElementById("loader")
 //outputs
+
 
 //global variable
 
 //functions
-const init = () => { }
+const init = () => {
+    close()
+}
+
+const open = () => {
+    modelContainer.classList.remove('close')
+    modelContainer.classList.add('open')
+    overlay.classList.add('overlay')
+    loader.classList.remove('open')
+    loader.classList.add('close')
+    contentInDialog.style.display="block"
+}
+
+const close = () => {
+    modelContainer.classList.remove('open')
+    modelContainer.classList.add('close')
+    overlay.classList.remove('overlay')
+    loader.classList.remove('open')
+    loader.classList.add('close')
+    contentInDialog.style.display="none"
+}
+
+const openLoader = () => {
+    modelContainer.classList.remove('close')
+    modelContainer.classList.add('open')
+    overlay.classList.add('overlay')
+    loader.classList.remove('close')
+    loader.classList.add('open')
+    contentInDialog.style.display="none"
+}
+
+const closeLoader = () => {
+    loader.classList.remove('open')
+    loader.classList.add('close')
+}
 
 //events
-
 // Add a click event listener to the button
-button.addEventListener('click', function () {
-
+button.addEventListener('click', () => {
+    // href="component/home.html"
+    if (email.value == "admin@example.com" && password.value == "qwerty") {
+        link.href = "component/home.html"
+    } else {
+        openLoader()
+        setTimeout(() => {
+            open()
+        }, 2000);
+    }
 });
+
+closeBtn.addEventListener("click", () => {
+    close()
+})
 
 //initial settings
 init();
